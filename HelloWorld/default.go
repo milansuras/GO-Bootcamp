0ea717d8e1b875801f900dfe27858ee446101981
MILAN_SURAS <milanmidhun2@gmail.com>
2024-01-27 09:18:51 +0530

    variable declaration in go

---
+++ HelloWorld/default.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	var x int   // declared variable x as type int by default its zero
+	var y bool  // declared variable as z type bool default value is false
+	var name string
+
+	fmt.Println("x", x)
+	fmt.Println("y", y)
+	fmt.Println("name", name)
+
+}
