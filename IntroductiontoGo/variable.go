2561350660fa632e23aa4e8f260103b86a60c5f5
MILAN_SURAS <milanmidhun2@gmail.com>
2024-01-30 21:55:56 +0530

    Variable in Golang

---
+++ IntroductiontoGo/variable.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var age int // declare as explicit 
+	age=10 //initalizing value to a variable
+
+	fmt.Println(age)
+
+	roll_no:=7 // short variable declaration 
+	fmt.Println(roll_no)
+
+
+}
