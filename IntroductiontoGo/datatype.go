487acab6312ea110a6590541841434fc00a58c20
MILAN_SURAS <milanmidhun2@gmail.com>
2024-01-30 21:50:16 +0530

    Datatype in Golang

---
+++ IntroductiontoGo/datatype.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+       var name string // by default it will be an empty string ""
+	   fmt.Println(name)
+	   
+	   name="GOLANG"
+	   fmt.Println(name)
+
+	   var a int // By default it will print zero
+	   fmt.Println(a)
+	   a=777
+	   fmt.Println(a)
+
+	   var b bool // By default it will print false
+	   fmt.Println(b)
+
+}
+
