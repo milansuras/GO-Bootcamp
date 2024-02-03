059ceca9a1c0fe3162cd46fb074eaa5571bc5a9f
MILAN_SURAS <milanmidhun2@gmail.com>
2024-02-03 12:51:02 +0530

    Sum of even number between ranges

--- Loops/sumEven.go
+++ Loops/sumEven.go
@@ -7,11 +7,15 @@ func main(){
 	var sum int
 	var r int
 
-	fmt.Println("Enter an number to find the sum of even number between ranges")
+	fmt.Println("Enter an number to find the sum of even number between ranges: ")
 	fmt.Scan(&r)
 
 	for i:=0; i<=r; i+=2{
-		sum+=sum
+
+		sum+=i
 	}
+
+	fmt.Println("The sum of even number between ranges =",sum)
 }
+
 
