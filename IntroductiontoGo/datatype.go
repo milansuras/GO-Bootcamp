fdc4278682e1a6e61ca0ae90e402686e9f00fc42
MILAN_SURAS <milanmidhun2@gmail.com>
2024-02-05 16:42:31 +0530

    Datatype redeclaration in Golang

--- IntroductiontoGo/datatype.go
+++ IntroductiontoGo/datatype.go
@@ -16,6 +16,20 @@ func main(){
 
 	   var b bool // By default it will print false
 	   fmt.Println(b)
+ 
+
+	   // re declaration concept
+
+	   x:= 10
+//	   x:=20
+
+	   fmt.Println(x) // here it will print nothing we cannot redecalre but using short varible
+	   // in-order o declare atleast a new variable should be introduced
+
+	   y:=777
+	   y, z:= 9,10
+
+	   fmt.Println(y,z)
 
 }
 
