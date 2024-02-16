64a7bb2c5db2737a40111b4f6ce90986048f517f
MILAN_SURAS <milanmidhun2@gmail.com>
2024-02-16 21:53:26 +0530

    Redeclaration concept

--- IntroductiontoGo/Redeclaration.go
+++ IntroductiontoGo/Redeclaration.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+func main() {
+	//declaring a variable
+	var a int
+	fmt.Println(a) // by default it will print zero
+
+	//re declaring
+	a = 1
+	fmt.Println(a)
+
+	//redeclaring concept in short variable declaration
+
+	b := 0
+	fmt.Println(b)
+
+	//b :=2 it will give an error. in order to redeclare a short variable at least a new variable should introduced
+	fmt.Println(b)
+
+	b, x := 3, 4
+	fmt.Println(b, x)
+}
