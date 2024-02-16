5899769739a9d7bf51f0d5694d16de4fe1ffbb0e
MILAN_SURAS <milanmidhun2@gmail.com>
2024-02-16 21:52:43 +0530

    Logical operator example

--- IntroductiontoGo/LogicalOperator.go
+++ IntroductiontoGo/LogicalOperator.go
@@ -1,1 +1,24 @@
 package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	var b int = 20
+
+	//AND OPERATOR
+	if a != b && a <= b { //both condition should be true
+		fmt.Println(true)
+	}
+
+	// OR
+
+	if a != b || a <= b {
+		fmt.Println(true) //either one of the condition to be true
+	}
+
+	//NOT
+	if !(a == b) {
+		fmt.Println(true)
+	}
+}
