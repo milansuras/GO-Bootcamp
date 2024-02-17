c96739abe4ba1a0183894996ee03f08f64614fe3
MILAN_SURAS <milanmidhun2@gmail.com>
2024-02-17 07:43:01 +0530

    Scope example in golang

--- IntroductiontoGo/Scope.go
+++ IntroductiontoGo/Scope.go
@@ -1,1 +1,31 @@
 package main
+
+import "fmt"
+
+var a int = 1
+
+//b:=2 // we cannot declare a short variable outside main function
+
+func main() {
+	fmt.Println(a)
+	//fmt.Println(b)
+	var x int = 11
+
+	{
+		x := 12
+		fmt.Println(x)
+		a = 3
+		fmt.Println(a)
+
+		var c int = 4
+		fmt.Println(c)
+
+		d := 10
+		fmt.Println(d)
+	}
+	fmt.Println(a)
+	//fmt.Println(d)
+	//fmt.Println(c) // both c  and d will be  avaialable in that block
+
+	fmt.Println(x)
+}
