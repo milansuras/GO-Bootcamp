b8647f38b7f0342ee4ff24bcdb8ba9bff30232a9
MILAN_SURAS <milanmidhun2@gmail.com>
2024-02-26 23:15:29 +0530

    Struct example

---
+++ Struct/Struct02.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type City_List struct {
+	city    string
+	state   string
+	country string
+}
+
+func main() {
+	var c1 City_List = City_List{"Vattiyoorkavu", "kerala", "India"}
+	fmt.Println(c1.state)
+	var c2 City_List = City_List{city: "Mumbai", state: "Maharashtra", country: "India"}
+	fmt.Println(c2)
+	fmt.Println(c2.country)
+	var c3 City_List
+
+	fmt.Println(c3.city)
+}
