6bfa532930a91c23350e8efcea1cf485a31c6460
MILAN_SURAS <milanmidhun2@gmail.com>
2024-03-09 21:37:26 +0530

    Printf

---
+++ IntroductiontoGo/printf.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    name := "Go"
+    value := 777
+    float_value := 12.23
+
+    fmt.Printf("Name %s :=\n", name)
+    fmt.Printf("value %d :=\n", value)
+    fmt.Printf("float value %f :=\n", float_value)
+}
