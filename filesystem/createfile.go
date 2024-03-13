2c5f7ddd13ba15714145f66a89c60beec2d7fb3b
MILAN_SURAS <milanmidhun2@gmail.com>
2024-03-13 22:30:07 +0530

    Create a file in go

---
+++ filesystem/createfile.go
@@ -0,0 +1,16 @@
+package main
+
+import ("fmt"
+"os")
+
+func main(){
+	file,err := os.Create("example.txt")
+	if err!=nil{
+		fmt.Println("Error creating file:",err)
+		return
+
+	}
+
+	defer file.Close()
+
+}
